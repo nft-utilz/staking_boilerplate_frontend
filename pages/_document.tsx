f0c53845b133491ae5ee9bf174958317c8d36f38
abehan7 <hanjk13262@gmail.com>
2022-06-22 00:14:17 +0900

    fix:> font flickering

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,23 @@
-import Document, { DocumentContext } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
-// 오류날 때
+const BASE_URL = "https://www.novmint.xyz";
+
 export default class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  //--------------For styled-components only------------//
+  static async getInitialProps(ctx: any) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
+          enhanceApp: (App: any) => (props: any) =>
             sheet.collectStyles(<App {...props} />),
         });
-      //
+
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        //FIXME: 어레이로 바꾸니까 된다 매우 중요 12323
         styles: [
           <>
             {initialProps.styles}
@@ -27,5 +28,54 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+  //---------------------------------------------------//
+  render() {
+    return (
+      <Html lang="en">
+        {/* 여기서 이렇게 preload하니까 플리커링 없어짐 */}
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/Montserrat-Black.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/Montserrat-Medium.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/NotoSansKR-Regular.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/Montserrat-Bold.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/fonts/Montserrat-Black.ttf"
+            as="font"
+            type="font/ttf"
+            crossOrigin=""
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
